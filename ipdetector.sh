8a5515bc0c5bfd0f6e3ac7f78cb896cdeab838dc
Vlad Rakhmanin <vrakhmanin1@gmail.com>
2021-12-28 09:45:53 +0000

    added comments

--- ipdetector.sh
+++ ipdetector.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Check if correct parameters, if not, error message
 if [ "$#" -ne 1 ]; then
     echo "Sorry, there is a parameters problem.">&2
-    echo "Syntax: ./ipdetector.sh [XXX.XXX.X]"
+    echo "Syntax: ./ipdetector.sh [XXX.XXX.X]">&2
     exit 1
 fi
 
-
+# Cycle through all available IP addresses
 for ip in {1..254..1}; do
 
+    # Ping current address, check if online, get the address from output stream, remove colon
     ping -c 1 "$1"."$ip" | grep "64 bytes" | cut -d" " -f4 | sed "s/://"&
 
 done
